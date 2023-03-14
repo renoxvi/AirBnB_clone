de415c28329aa7dd507a38e00287471e00d1cec8
renoxvi <vicreno08@gmail.com>
2023-03-14 15:07:19 -0700

    bnb

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -72,7 +72,7 @@ class TestState(unittest.TestCase):
         self.new_state.name = "Tunis"
         dict_rep = self.new_state.to_dict()
         list = ['id', 'created_at', 'updated_at',
-                    'name', '__class__']
+        'name', '__class__']
         num = 0
         for att in dict_rep.keys():
             if att in list:
@@ -96,6 +96,7 @@ class TestState(unittest.TestCase):
             os.remove("objects.json")
         except BaseException:
             pass
+
 
 if __name__ == '__main__':
     unittest.main()
